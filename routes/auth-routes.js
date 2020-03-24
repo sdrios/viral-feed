ce0a7ed3228c62be66abfc0b46423e1e4b6181ac
Sarah <sarahd.rios@gmail.com>
2020-03-24 18:20:16 -0500

    add secret

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,70 +1,70 @@
-const router = require('express').Router();
-const passport = require('passport');
-const path = require('path');
-const models = require('../models');
-const TwitterStrategy = require('passport-twitter').Strategy;
-require('dotenv').config();
+// const router = require('express').Router();
+// const passport = require('passport');
+// const path = require('path');
+// const models = require('../models');
+// const TwitterStrategy = require('passport-twitter').Strategy;
+// require('dotenv').config();
 
 
-//Twitter Auth
-router.get('/twitter',passport.authenticate('twitter'));
+// //Twitter Auth
+// router.get('/twitter',passport.authenticate('twitter'));
 
-//Twitter Callback
-router.get('/twitter/callback',
- passport.authenticate('twitter',
- {successRedirect:'/',
- failureRedirect:'/auth/twitter/failed'}));
+// //Twitter Callback
+// router.get('/twitter/callback',
+//  passport.authenticate('twitter',
+//  {successRedirect:'/',
+//  failureRedirect:'/auth/twitter/failed'}));
 
 
-//Twitter Success - COMPLETE WHEN REDIRECT ROUTE IS BUILT 
-//(display tweet that user wants to like to retweet)
-// router.get('/twitter/success', (req,res)=>{
-// console.log('AUTH SUCCESS ROUTE')
-//  res.sendFile(path.join(__dirname,'build/index.html'));
+// //Twitter Success - COMPLETE WHEN REDIRECT ROUTE IS BUILT 
+// //(display tweet that user wants to like to retweet)
+// // router.get('/twitter/success', (req,res)=>{
+// // console.log('AUTH SUCCESS ROUTE')
+// //  res.sendFile(path.join(__dirname,'build/index.html'));
+// // })
+
+// //Twitter Failed
+// router.get('/twitter/failed', (req,res)=>{
+// console.log('AUTH FAILED ROUTE');
+// res.send('AUTH FAILED');
 // })
 
-//Twitter Failed
-router.get('/twitter/failed', (req,res)=>{
-console.log('AUTH FAILED ROUTE');
-res.send('AUTH FAILED');
-})
+// // Configure the Twitter strategy for use by Passport.
 
-// Configure the Twitter strategy for use by Passport.
-
-passport.use(new TwitterStrategy({
-    consumerKey: process.env['TWITTER_CONSUMER_KEY'],
-    consumerSecret: process.env['TWITTER_CONSUMER_SECRET'],
-    callbackURL: '/auth/twitter/callback'
-},
-    function (token, tokenSecret, profile, cb) {
-        console.log("USER AUTHENTICATED!")
-        //console.log(profile);
-        models.userInfo.findOrCreate({ 
-            where:{
-                userID: profile.id,
-                userName: profile.username,
-                displayName: profile.displayName
-                } 
-                }).then ((user, err) =>{
-                    if (err){
-                        console.log("ERROR IF")
-                        return cb(err);
-                    }
-                    console.log("OUTSIDE ERROR IF")
-                    cb(null,user);
-        });
-    }
-    ));
+// passport.use(new TwitterStrategy({
+//     consumerKey: process.env['TWITTER_CONSUMER_KEY'],
+//     consumerSecret: process.env['TWITTER_CONSUMER_SECRET'],
+//     callbackURL: '/auth/twitter/callback'
+// },
+//     function (token, tokenSecret, profile, cb) {
+//         console.log("USER AUTHENTICATED!")
+//         //console.log(profile);
+//         models.userInfo.findOrCreate({ 
+//             where:{
+//                 userID: profile.id,
+//                 userName: profile.username,
+//                 displayName: profile.displayName
+//                 } 
+//                 }).then ((user, err) =>{
+//                     if (err){
+//                         console.log("ERROR IF")
+//                         return cb(err);
+//                     }
+//                     console.log("OUTSIDE ERROR IF")
+//                     cb(null,user);
+//         });
+//     }
+//     ));
 
 
-// Configure Passport authenticated session persistence.
-passport.serializeUser(function (user, cb) {
-    cb(null, user);
-});
+// // Configure Passport authenticated session persistence.
+// passport.serializeUser(function (user, cb) {
+//     cb(null, user);
+// });
 
-passport.deserializeUser(function (obj, cb) {
-    cb(null, obj);
-});
+// passport.deserializeUser(function (obj, cb) {
+//     cb(null, obj);
+// });
 
 
-module.exports = router;
+// module.exports = router;
--- server.js
+++ server.js
@@ -20,12 +20,11 @@ app.use(passport.session());
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use('/auth', authRoutes); //include auth routes
+//app.use('/auth', authRoutes); //include auth routes
 
 app.use(express.static(path.join(__dirname,'build')));
 
 app.get('/', (req,res)=>{
-    console.log('star route')
     res.sendFile(path.join(__dirname,'build/index.html'));
 })
 
